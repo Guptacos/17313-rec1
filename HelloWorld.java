e97af3e08bca0ce190c8228d498d9a55c5a08cf6
Niko Gupta <niko.gupta@gmail.com>
2019-08-28 14:02:08 -0400

    added my name to the Hello World file

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ *  @name Niko Gupta
+ */
+
+public final class Main {
+  private static final String toPrint = "Hello World! My name is Niko";
+
+  private Main() {
+  }
+
+  public static void main(String[] args) {
+    system.out.println(toPrint);
+  }
+}
