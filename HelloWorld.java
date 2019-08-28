cf2538a38858ddff5cf2dae9b444279ad09dcfa3
Niko Gupta <niko.gupta@gmail.com>
2019-08-28 14:03:13 -0400

    added how many years I've been at CMU to the Hello World file

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ *  @name Niko Gupta
+ */
+
+public final class Main {
+  private static final String toPrint = "Hello World! I've been at CMU for 3 years.";
+
+  private Main() {
+  }
+
+  public static void main(String[] args) {
+    system.out.println(toPrint);
+  }
+}
